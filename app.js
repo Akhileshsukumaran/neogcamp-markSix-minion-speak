4c6e529227a028ff568de9b34ad8ba3ce4aefc2a
Akhileshsukumaran <akhileshs2017@gmail.com>
2021-08-24 10:54:03 +0530

    updated script

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert("hello!");
+var buttonClick = document.querySelector("#btn-click");
+
+var inputTxt = document.querySelector("#txt-input");
+
+buttonClick.addEventListener("click", function clickEventHandler(){
+    alert("clicked " + inputTxt.value)
+})

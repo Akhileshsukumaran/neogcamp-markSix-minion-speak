e940d517f2d3324dace9039eb1a26ac95597dd22
Akhileshsukumaran <akhileshs2017@gmail.com>
2021-08-24 12:49:31 +0530

    major code changes

--- app.js
+++ app.js
@@ -4,6 +4,29 @@ var inputTxt = document.querySelector("#txt-input");
 
 var outputTxt = document.querySelector(".output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+// error handler function
+function errorHandler(error){
+    console.log("error occured", error);
+}
+
+//function to get converted text
+function getUrl(text){
+    return fetch(serverURL + "?text=" + text)
+    .then(response => response.json())
+    // .then(json => console.log(json))
+    // .then(json => console.log(json.contents.translated))
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputTxt.innerText = translatedText; //writes the translated text to the output div
+    })
+    .catch(errorHandler);
+}
+
+//button click event
 buttonClick.addEventListener("click", function clickEventHandler(){
-    outputTxt.innerHTML = "hello " + inputTxt.value;
+    
+    getUrl(inputTxt.value);
+
 })
